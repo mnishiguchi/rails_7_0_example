253f89f0ee8883f04b0ef2639fff60ccbf092f5c
Masatoshi N <nishiguchi.masa@gmail.com>
2021-11-29 12:09:55 -0500

    Implement messages_controller and adjust etc

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :rooms
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # Almost every application defines a route for the root path ("/") at the top of this file.
-  # root "articles#index"
+  root "rooms#index"
+
+  resources :rooms do
+    resources :messages, only: %i[new create]
+  end
 end
